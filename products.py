1d8184cac06a53bebd32542beae87702ff2b3743
LamyaDev <Raindrob@outlook.sa>
2025-11-19 08:35:43 +0300

    Delete products.py

--- products.py
+++
@@ -1,7 +0,0 @@
-products = [
-    {"name": "Coffee", "price": 18, "quantity": 3},
-    {"name": "Tea", "price": 9, "quantity": 10},
-    {"name": "Milk", "price": 6, "quantity": 5},
-    {"name": "Juice", "price": 12, "quantity": 2},
-    {"name": "Water", "price": 3, "quantity": 30}
-]
